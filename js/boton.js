f429e1de06526291c3e2c1d3f5618b5ec6c0a449
elrickshernandez <99761410+elrickshernandez@users.noreply.github.com>
2022-05-13 15:40:09 -0500

    Add files via upload

---
+++ js/boton.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#btonAgregar").button();
+    $("#btonAgregar").click(function(){
+
+    });
+    $("#btnEnviar").button();
+    $("#btnEnviar").click(function(){
+
+    });
+    $("#btnModificar").button();
+    $("#btnModificar").click(function(){
+
+    });
+    $("#btnFiltrar").button();
+    $("#btnFiltrar").click(function(){
+
+    });
+    $("#btnRegis").button();
+    $("#btnRegis").click(function(){
+
+    });
+    $("#btnPedido").button();
+    $("#btnPedido").click(function(){
+
+    });
+    $("#elim").button();
+    $("#elim").click(function(){
+
+    });
+    $("#mod").button();
+    $("#mod").click(function(){
+
+    });
+    $("#btnCalcular").button();
+    $("#btnCalcular").click(function(){
+
+    });
+    $("#btnRealizarp").button();
+    $("#btnRealizarp").click(function(){
+
+    });
+$("#cmbPuesto").selectmenu();
+$("#cmbTipo").selectmenu();
+$("#sabores").selectmenu();
+$("#pan").selectmenu();
+$( "#datepicker" ).datepicker();
+});
